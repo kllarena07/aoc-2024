96830b6b9ee00251f292d1b81a4a37b53683f273
Kieran Llarena <baymax709@gmail.com>
2024-12-04 21:15:29 -0500

    delete day4/target directory

--- day4/target/debug/day4.d
+++
@@ -1,1 +0,0 @@
-/Users/kllarena/Documents/GitHub/aoc-2024/day4/target/debug/day4: /Users/kllarena/Documents/GitHub/aoc-2024/day4/src/main.rs
--- day4/target/debug/deps/day4-f05a915e687676f4.d
+++
@@ -1,5 +0,0 @@
-/Users/kllarena/Documents/GitHub/aoc-2024/day4/target/debug/deps/day4-f05a915e687676f4: src/main.rs
-
-/Users/kllarena/Documents/GitHub/aoc-2024/day4/target/debug/deps/day4-f05a915e687676f4.d: src/main.rs
-
-src/main.rs:
