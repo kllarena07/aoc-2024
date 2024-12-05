8bb9a96b8c048913f24432cdc51a2ae59b79d1b6
Kieran Llarena <baymax709@gmail.com>
2024-12-04 21:26:14 -0500

    feat: read input.txt

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,8 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file_contents = fs::read_to_string("./input.txt").unwrap();
+    let trimmed_contents = file_contents.trim_end();
+
+    println!("{}", trimmed_contents);
 }
